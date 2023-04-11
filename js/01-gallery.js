fca8030396e395d9b248f660a790db5ce39f71d1
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-04-11 22:27:01 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createMarkup(galleryItems) {
+    const markup = galleryItems.map(({preview, original, description}) => {
+        
+        return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </li>`;
+});
+
+console.log(markup);
+
+};
+
+
+
+
